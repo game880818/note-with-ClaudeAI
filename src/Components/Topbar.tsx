392bb4b05420d47f361efe971df5de2082843584
Syu <game880818@gmail.com>
2026-06-17 20:01:26 +0900

    add Google Login & LogOut Button in topbar Component

--- src/Components/Topbar.tsx
+++ src/Components/Topbar.tsx
@@ -1,5 +1,6 @@
 // TODO Day3: AI パネルの開閉ボタンを機能させる
 import { formalFullTimeJa } from "../utils/formalTime"
+import supabase from "../../lib/supabase"
 
 interface topbarProps {
   title: string
@@ -11,6 +12,11 @@ interface topbarProps {
 }
 
 export function Topbar({ title, updatedAt, aiOpen, onAiToggle, onDelete, hasNote }: topbarProps) {
+  async function handleLogin() {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    })
+  }
   return (
     <header className="topbar">
 
@@ -35,6 +41,14 @@ export function Topbar({ title, updatedAt, aiOpen, onAiToggle, onDelete, hasNote
         >
           🗑
         </button>}
+
+      <button className="topbar-auth-btn" >
+        ログアウト
+      </button>
+
+      <button className="topbar-auth-btn login" >
+        Googleでログイン
+      </button>
 
     </header>
   )
